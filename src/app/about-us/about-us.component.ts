ae0718eebe272743b55602413b29563d5eada280
Sabarna <choudhurysabarna96745@gmail.com>
2020-11-23 11:18:43 +0530

    added responsive behaviour

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
-
+ 
+  medha : string = "assets/images/medhashree.jpg";
+  sabu : string = "assets/images/sabarna.jpg";
+  udit : string = "assets/images/udit.png";
+ 
   constructor() { }
-
+ 
   ngOnInit(): void {
   }
-
+ 
 }
